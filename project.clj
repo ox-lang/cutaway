f9a128c3907e5639fc45d5d90d4bb6f8c4fe1316
arrdem <me@arrdem.com>
2014-06-18 17:39:56 -0500

    Fix up details

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject me.arrdem.cutaway "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject me.arrdem/cutaway "0.1.0-SNAPSHOT"
+  :description "Fast(er) nested structure updates for Clojure"
+  :url "http://github.com/arrdem/cutaway"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
